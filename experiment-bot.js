226ca98175bd104974106c924f81cd13af88d129
Dean Pooler <Deanpooler@outlook.com>
2020-04-16 16:56:31 +0200

    Add comments

--- experiment-bot.js
+++ experiment-bot.js
@@ -1,12 +1,17 @@
+// Dependencies
 require('dotenv').config();
 const Discord = require('discord.js');
+// Instantiate new discord bot
 const bot = new Discord.Client();
 
+// Get bot token from dotenv file
 const TOKEN = process.env.TOKEN;
 
+// Log the bot in with given credentials
 bot.login(TOKEN);
 
+// When bot is ready log message to console
 bot.on('ready', () => {
-    console.info(`Logged in as ${bot.user.tag}!`);
+	console.info(`Logged in as ${bot.user.tag}!`);
 });
 
