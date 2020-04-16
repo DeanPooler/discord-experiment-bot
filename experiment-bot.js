1cdb737f39c34cdbe50451e3881361e3c34f6d9a
Dean Pooler <Deanpooler@outlook.com>
2020-04-16 17:16:25 +0200

    Add on message function

--- experiment-bot.js
+++ experiment-bot.js
@@ -1,20 +1,34 @@
 // Dependencies
 require('dotenv').config();
 const Discord = require('discord.js');
-// Instantiate new discord bot
-const bot = new Discord.Client();
+// Instantiate new discord client
+const client = new Discord.Client();
 
-// Get bot token from dotenv file
+// Get client token from dotenv file
 const TOKEN = process.env.TOKEN;
 
-// Log the bot in with given credentials
-bot.login(TOKEN);
+// Log the client in with given credentials
+client.login(TOKEN);
 
-// When bot is ready log message to console
+// When client is ready log message to console
 // Can only be triggered once
-bot.once('ready', () => {
-	console.info(`Logged in as ${bot.user.tag}!`);
+client.once('ready', () => {
+	console.info(`Logged in as ${client.user.tag}!`);
 });
 
-// When bot reads a message
+// When client reads a message check if the right prefix has been used
+// If prefix is found check against cases
+// If command is matched run that case's code
+client.on('message', message => {
+	if (message.charAt(0) == '!') {
+		console.log(message.content);
+		switch (message.content) {
+		case '!ping':
+			message.channel.send('pong!');
+			break;
 
+		default:
+			break;
+		}
+	}
+});
